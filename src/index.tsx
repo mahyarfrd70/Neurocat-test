063d6fb30648612f3d0a3a2be871215b9d6ac646
mahyar.fard <mahyar.fard@snapptrip.com>
2021-02-01 11:17:33 +0330

    remove unused ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
 import App from "./containers/App";
 import GlobalStyles from "./globalStyles";
 import reportWebVitals from "./reportWebVitals";
